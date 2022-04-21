b561c27ab9f003e96011691cd8e2a833a6e48a54
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-04-21 14:52:36 +0700

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pwa: withPWA({
+    dest: "public",
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "google-fonts-stylesheets",
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 60 * 60 * 24 * 30, // 1 month
+          },
+        },
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "google-fonts-webfonts",
+          expiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 60 * 60 * 24 * 30, // 1 month
+          },
+        },
+      },
+    ],
+  }),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
