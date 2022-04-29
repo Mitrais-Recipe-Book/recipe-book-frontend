ed04eb4e48040a19f81e9ab93220dd26b46f8065
Naufal Ibrahim <naufal.ti@outlook.com>
2022-04-29 09:08:26 +0700

    Add create new tag; styling admin dashboard

--- components/Admin/TagsTable.tsx
+++ components/Admin/TagsTable.tsx
@@ -2,64 +2,74 @@ import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 
 export default function TagsTable() {
-    const [tags, setTags] = useState([{
-        tag: "Tag 1",
-      },
-      {
-        tag: "Tag 2",
-      },
-      {
-        tag: "Tag 3",
-      },])
+  const [tags, setTags] = useState([
+    {
+      tag: "Tag 1",
+    },
+    {
+      tag: "Tag 2",
+    },
+    {
+      tag: "Tag 3",
+    },
+  ]);
+
+  const [newTag, setNewTag] = useState("");
 
   const columns = [
     {
-      name: "Tag",
+      name: "Tags",
       sortable: true,
       selector: (row: { tag: any }) => (
         <input
           disabled={true}
           name={"input" + row.tag}
-          className="text-center"
+          className="text-base p-1 rounded-full w-full"
           type="text"
           value={row.tag}
           onChange={(e) => {
-              setTags(tags.map(tag => {
-                    if (tag.tag === row.tag) {
-                        tag.tag = e.target.value
-                    }
-                    return tag
-                }))
-            }}
+            setTags(
+              tags.map((tag) => {
+                if (tag.tag === row.tag) {
+                  tag.tag = e.target.value;
+                }
+                return tag;
+              })
+            );
+          }}
         />
       ),
-      
     },
     {
       name: "Actions",
       sortable: false,
+      maxWidth: "100px",
       selector: (row: { tag: any }) => {
         return (
           <div>
             <button
               name={"edit" + row.tag}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
               onClick={() => {
                 var inputText = document.querySelector(
                   `input[name="input${row.tag}"]`
                 ) as HTMLInputElement;
                 inputText.disabled = !inputText.disabled;
                 inputText.disabled
-                  ? (inputText.className =
-                      "border-0 border-gray-400 p-1 rounded-full text-center",
-                      (document.querySelector(
+                  ? ((inputText.className =
+                      "w-full border-0 p-1 rounded-full text-base"),
+                    ((
+                      document.querySelector(
                         `button[name="edit${row.tag}"]`
-                      ) as HTMLButtonElement).innerHTML = "Edit")
-                  : (inputText.className =
-                      "border-2 border-gray-400 p-1 rounded-full text-center",
-                      (document.querySelector(
+                      ) as HTMLButtonElement
+                    ).innerHTML = "Edit"))
+                  : ((inputText.className =
+                      "w-full border-2 border-gray-400 p-1 rounded-full text-base"),
+                    ((
+                      document.querySelector(
                         `button[name="edit${row.tag}"]`
-                      ) as HTMLButtonElement).innerHTML = "Save");
-
+                      ) as HTMLButtonElement
+                    ).innerHTML = "Save"));
               }}
             >
               Edit
@@ -71,16 +81,37 @@ export default function TagsTable() {
   ];
 
   return (
-    <div>
-      <DataTable
-        title="Tags"
-        //@ts-ignore
-        columns={columns}
-        data={tags}
-        pagination
-        paginationPerPage={5}
-        paginationRowsPerPageOptions={[5, 10, 20]}
-      />
+    <div className="px-4 py-4 m-2">
+        <DataTable
+          name="Tags"
+          //@ts-ignore
+          columns={columns}
+          data={tags}
+          pagination
+          paginationPerPage={5}
+          paginationRowsPerPageOptions={[5, 10, 20]}
+        />
+      <div className="flex flex-wrap px-4">
+        <input
+          className="w-1/2 border-2 border-gray-400 p-1 rounded-full text-center"
+          type="text"
+          placeholder="New tag..."
+          value={newTag}
+          onChange={(e) => {
+            setNewTag(e.target.value);
+          }}
+        />
+        <span className="px-2"></span>
+        <button
+          className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-1 px-4 rounded"
+          onClick={() => {
+            setTags([...tags, { tag: newTag }]);
+            setNewTag("");
+          }}
+        >
+          Add
+        </button>
+      </div>
     </div>
   );
 }
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import TagsTable from '../../components/Admin/TagsTable'
+import React from "react";
+import TagsTable from "../../components/Admin/TagsTable";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
 
 export default function Index() {
   return (
     <div>
-        <TagsTable />
+      <Navbar />
+      <main className="container mx-auto pt-1">
+        <div className="container w-3/4 mx-auto pt-1">
+          <section className="my-2 py-3 rounded-md bg-white drop-shadow-lg">
+            <h1 className="text-4xl text-center mb-3 font-bold">
+              Admin Dashboard
+            </h1>
+          </section>
+          <section className="my-2 py-3 rounded-md bg-white drop-shadow-lg">
+          <TagsTable />
+            </section>
+        </div>
+        <Footer />
+      </main>
     </div>
-  )
+  );
 }
