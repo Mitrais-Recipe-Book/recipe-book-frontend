b835c8ce74f31fe17e59b4ff0dee4ecde948fd6f
Naufal Ibrahim <naufal.ti@outlook.com>
2022-05-10 10:26:02 +0700

    fix navbar not change page properly

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="./" className="flex items-center">
+        <a href="/" className="flex items-center">
           <Image
             src="/images/bibimbap192x192.png"
             alt="RecipyBook"
@@ -150,7 +150,7 @@ export default function Navbar() {
             </li>
             <li>
               <a
-                href="profile"
+                href="/profile"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Profile
