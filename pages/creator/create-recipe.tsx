be6187392183c4b5bcf07e12be25ff0b69abcc79
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-05-12 12:29:16 +0700

    update recipeform

--- pages/creator/create-recipe.tsx
+++ pages/creator/create-recipe.tsx
@@ -7,8 +7,13 @@ import { IoIosAdd, IoIosRemove } from "react-icons/io";
 
 export default function CreateRecipe() {
   const [userInfo, setUserInfo]: any = useState({});
-  const [recipeForm, setRecipeForm]: any = useState({});
+  const [recipeForm, setRecipeForm]: any = useState({
+    draft: true,
+  });
   const [ingredientList, setIngredientList]: any = useState([]);
+  const [submit, setSubmit]: any = useState(false);
+  const [imageFormData, setImageFormData]: any = useState({});
+  const [recipeTags, setRecipeTags]: any = useState([]);
   const username = "user1";
 
   const onAddBtnClick = () => {
@@ -44,13 +49,73 @@ export default function CreateRecipe() {
       .catch((err) => {
         console.log(err);
       });
+    axios
+      .get("https://recipyb-dev.herokuapp.com/api/v1/tag")
+      .then((res) => {
+        setRecipeTags(res.data.payload);
+        console.log("Tags: ", res.data.payload);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
+
+  useEffect(() => {
+    if (submit) {
+      console.log(recipeForm);
+      // handleSubmit();
+      setSubmit(false);
+    }
+  }, [submit, recipeForm]);
+
+  function handleSubmit() {
+    axios
+      .post("https://recipyb-dev.herokuapp.com/api/v1/recipe/add", recipeForm)
+      .then((res) => {
+        uploadImage(res.data.payload.recipeId);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function uploadImage(recipeId: any) {
+    const formData = new FormData();
+    formData.append(
+      recipeForm.title + "-photo",
+      imageFormData,
+      imageFormData.name
+    );
+    axios
+      .put(
+        "https://recipyb-dev.herokuapp.com/api/v1/recipe/" +
+          recipeId +
+          "/photo",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      )
+      .then((res) => {
+        // console.log(res);
+        // setModalImage(false);
+        setImageFormData({});
+        window.alert("Successfully updated");
+        // dispatch(setUserImage());
+      })
+      .catch((err) => {
+        console.log(err);
+        window.alert("Failed to update");
+      });
+  }
 
   return (
     <>
       <Navbar />
       <div
-        className="py-6 h-[90vh] bg-repeat bg-contain
+        className="py-6 bg-repeat bg-contain
       bg-[url('https://previews.123rf.com/images/redspruce/redspruce1409/redspruce140900015/32280224-doodle-food-icons-seamless-background.jpg')]"
       >
         <div className="relative py-3 w-full mx-auto">
@@ -78,6 +143,9 @@ export default function CreateRecipe() {
                       type="text"
                       className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
                       placeholder="Recipe Title"
+                      onChange={(e) => {
+                        setRecipeForm({ ...recipeForm, title: e.target.value });
+                      }}
                     />
                   </div>
                   <div className="flex flex-col">
@@ -86,6 +154,12 @@ export default function CreateRecipe() {
                       type="text"
                       className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
                       placeholder="Recipe Overview"
+                      onChange={(e) => {
+                        setRecipeForm({
+                          ...recipeForm,
+                          overview: e.target.value,
+                        });
+                      }}
                     />
                   </div>
                   <div className="flex flex-col">
@@ -95,6 +169,12 @@ export default function CreateRecipe() {
                         type="text"
                         className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-11/12 sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
                         placeholder="Ingredient name and quantity"
+                        onChange={(e) => {
+                          setRecipeForm({
+                            ...recipeForm,
+                            ingredients: e.target.value,
+                          });
+                        }}
                       />
                       <div className="flex items-center align-middle justify-center">
                         <IoIosAdd
@@ -112,18 +192,26 @@ export default function CreateRecipe() {
                     {ingredientList}
                   </div>
                   <div className="flex flex-col">
-                    <label className="leading-loose">
-                      Recipe Upload Image Banner
-                    </label>
+                    <label className="leading-loose">Upload Banner Image</label>
                     <input type="file" />
                   </div>
                   <div className="flex flex-col">
-                    <label className="leading-loose">VideoURL </label>
+                    <label className="leading-loose">VideoURL</label>
                     <input
-                      type="text"
+                      type={"url"}
                       className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
                       placeholder="Link to Video"
+                      onChange={(e) => {
+                        setRecipeForm({
+                          ...recipeForm,
+                          videoURL: e.target.value,
+                        });
+                      }}
                     />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="leading-loose"> Content</label>
+                    <textarea className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"></textarea>
                   </div>
                 </div>
                 <div className="pt-4 flex items-center space-x-4">
@@ -133,7 +221,16 @@ export default function CreateRecipe() {
                   <button className="bg-yellow-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">
                     Draft
                   </button>
-                  <button className="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">
+                  <button
+                    className="bg-blue-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none"
+                    onClick={() => {
+                      setRecipeForm({
+                        ...recipeForm,
+                        draft: false,
+                      }),
+                        setSubmit(true);
+                    }}
+                  >
                     Create
                   </button>
                 </div>
