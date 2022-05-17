2ccd8e752e68abd33bc953733bb9ca31a4a287f9
Naufal Ibrahim <naufal.ti@outlook.com>
2022-05-17 11:25:23 +0700

    Sort still change when value change

--- components/Admin/TagsTable.tsx
+++ components/Admin/TagsTable.tsx
@@ -1,24 +1,19 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DataTable from "react-data-table-component";
 
 export default function TagsTable() {
   const URL = "https://recipyb-dev.herokuapp.com/api/v1/tag";
+  const [loading, setLoading] = useState(true)
+  // const [sortable, setSortable] = useState(true)
+  const sortable = useRef(true)
   const [notif, setNotif] = useState(false);
   const [errorMessage, setErrorMessage] = useState("")
   const [tags, setTags] = useState([
     {
       id: 1,
-      name: "Tag 1",
-    },
-    {
-      id: 2,
-      name: "Tag 2",
-    },
-    {
-      id: 3,
-      name: "Tag 3",
-    },
+      name: "",
+    }
   ]);
 
   const [newTag, setNewTag] = useState("");
@@ -26,13 +21,24 @@ export default function TagsTable() {
   useEffect(() => {
     axios.get(URL).then((res) => {
       setTags(res.data.payload)
+      setLoading(false)
     });
   }, []);
 
   const columns = [
     {
       name: "Tags",
-      sortable: true,
+      sortFunction: (a: any,b: any) => {
+        if (sortable.current){
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+      }
+        return 0;
+      },
       selector: (row: { id: any, name: string }) => (
         <input
           disabled={true}
@@ -71,6 +77,7 @@ export default function TagsTable() {
                 inputText.disabled
                   ? ((inputText.className =
                       "w-full border-0 p-1 rounded-full text-base"),
+                      sortable.current=true,
                     axios.put(URL, {
                       tagId: row.id,
                       tagReplace: row.name})
@@ -83,7 +90,7 @@ export default function TagsTable() {
                         axios.get(URL).then((res) => {
                           setTags(res.data.payload);
                         });
-                        setErrorMessage(err.response.data.message);
+                        setErrorMessage(`Failed to edit tag.\n${err.response.data.message}`);
                         setNotif(true);
                       }
                       ),
@@ -94,6 +101,7 @@ export default function TagsTable() {
                     ).innerHTML = "Edit"))
                   : ((inputText.className =
                       "w-full border-2 border-gray-400 p-1 rounded-full text-base"),
+                    sortable.current=false,
                     ((
                       document.querySelector(
                         `button[name='edit${row.id}']`
@@ -115,7 +123,7 @@ export default function TagsTable() {
             <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
               <strong className="font-bold">Failed!</strong>
               <br />
-              <span className="block sm:inline">Failed created new tag, {errorMessage}</span>
+              <span className="block sm:inline">{errorMessage}</span>
               <span className="absolute top-0 bottom-0 right-0 px-4 py-3 " onClick={()=>setNotif(false)}>
                 <svg className="fill-current h-6 w-6 text-black-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
                  /></svg>
@@ -127,6 +135,7 @@ export default function TagsTable() {
         //@ts-ignore
         columns={columns}
         data={tags}
+        progressPending={loading}
         pagination
         paginationPerPage={5}
         paginationRowsPerPageOptions={[5, 10, 20]}
@@ -157,8 +166,7 @@ export default function TagsTable() {
               ]);
             })
             .catch((err) => {
-              console.log("e: ",err.response.data);
-              setErrorMessage(err.response.data.message);
+              setErrorMessage(`Failed to create tag.\n${err.response.data.message}`);
               setNotif(true);
             }
             );
