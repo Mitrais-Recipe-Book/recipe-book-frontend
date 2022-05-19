389c2d5c7d6671413640216891dc4c473f3e199f
Naufal Ibrahim <naufal.ti@outlook.com>
2022-05-19 10:51:13 +0700

    TagsPil now direct to search page

--- components/TagsPill.tsx
+++ components/TagsPill.tsx
@@ -1,39 +1,19 @@
-import  Router  from "next/router";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTagsToQuery, removeTagsFromQuery } from "../redux/reducers/queryReducer";
+import {addTagsToQuery,sendQuery,} from "../redux/reducers/queryReducer";
 
-
-const TagsPill = ({tag}:any) => {
+const TagsPill = ({ tag }: any) => {
   const dispatch = useDispatch();
-  const queryTags = useSelector((state:any) => state.query.queryTags);
-  const [isActive, setIsActive] = useState(false);
 
-  useEffect(() => {
-    // console.log("query",queryTags);
-    // setIsActive(queryTags.has(tag.id));
-  }, [queryTags]);
-  
   return (
-    <div id="tag" className="rounded py-1 px-4 mx-1 my-1 border-solid border-2 border-black transition-all cursor-pointer hover:scale-110 hover:bg-orange-300 "
-    onClick={(event) => {
-      // console.log(queryTags.has(tag.id));
-      setIsActive(queryTags.has(tag.id));
-      if(!isActive){
-      dispatch(addTagsToQuery(tag.id));
-      //@ts-ignore
-      dispatch(getRecipesByTags());
-      event.currentTarget.className = "rounded bg-orange-600 py-1 px-4 mx-1 my-1 border-solid border-2 border-black transition-all cursor-pointer hover:scale-110 hover:bg-orange-300 ";
-      // setIsActive(true);
-      } else{
-        dispatch(removeTagsFromQuery(tag.id));
+    <div
+      id="tag"
+      className="rounded py-1 px-4 mx-1 my-1 border-solid border-2 border-black transition-all cursor-pointer hover:scale-110 hover:bg-orange-300 "
+      onClick={(event) => {
+        dispatch(addTagsToQuery(tag));
         //@ts-ignore
-        dispatch(getRecipesByTags());
-        event.currentTarget.className = "rounded bg-white py-1 px-4 mx-1 my-1 border-solid border-2 border-black transition-all cursor-pointer hover:scale-110 hover:bg-orange-300";
-        // setIsActive(false);
-      }
-    }
-    }
+        dispatch(sendQuery());
+      }}
     >
       <h1 className="whitespace-nowrap">{tag.name}</h1>
     </div>
