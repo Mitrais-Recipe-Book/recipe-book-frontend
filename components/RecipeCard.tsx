a5ce5e3b0dd699a793f2dcb31380c6d2d8274ac3
Naufal Ibrahim <naufal.ti@outlook.com>
2022-05-25 08:24:39 +0700

    Fix not load default img for banner

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { SiCodechef } from "react-icons/si";
+import axios from "axios";
 
 // @ts-ignore
 export default function RecipeCard(props) {
   const [recipeImg, setRecipeImg] = useState("");
 
-  useEffect(() =>{
-    setRecipeImg(process.env.API_URL + `recipe/${props.recipe.id}/photo`)
-  },[props])
+  useEffect(() => {
+    axios
+      .get(process.env.API_URL + `recipe/${props.recipe.id}/photo`)
+      .then((res) => {
+        
+      })
+      .catch((err) => {
+        console.log(err.response.status);
+        if (err.response.status === 500) {
+          setRecipeImg(process.env.API_URL + `recipe/${props.recipe.id}/photo`);
+        } else{
+        setRecipeImg("/images/bibimbap-image.webp");
+        }
+      });
+    setRecipeImg(process.env.API_URL + `recipe/${props.recipe.id}/photo`);
+  }, [props]);
 
   return (
     <div className="mx-2 my-3 sm:w-40 xl:w-50 box-border border-1 pb-2 rounded shadow transition-all hover:bg-orange-200 hover:scale-110">
       <Image
         className="w-full rounded-t cursor-pointer"
-        src={recipeImg ? recipeImg : "/images/bibimbap-image.webp"}
+        src={recipeImg}
         alt="RecipyBook"
         width={200}
         height={130}
@@ -29,7 +43,18 @@ export default function RecipeCard(props) {
         </div>
         <div className="flex py-2">
           <div className="py-2 cursor-pointer ml-1 mr-2">
-            {props.recipe.authorImage ? <Image src={props.recipe.authorImage} className="rounded-full" width={24} height={24} objectFit="cover" alt="author profile" /> : <SiCodechef className="rounded-full" size={24} />}
+            {props.recipe.authorImage ? (
+              <Image
+                src={props.recipe.authorImage}
+                className="rounded-full"
+                width={24}
+                height={24}
+                objectFit="cover"
+                alt="author profile"
+              />
+            ) : (
+              <SiCodechef className="rounded-full" size={24} />
+            )}
           </div>
           <div>
             <div className="font-semibold text-sm cursor-pointer">
@@ -42,7 +67,10 @@ export default function RecipeCard(props) {
         </div>
       </div>
       <div className="text-center pt-1">
-        <div className="text-sm text-gray-600"> {props.recipe.recipeViews} </div>
+        <div className="text-sm text-gray-600">
+          {" "}
+          {props.recipe.recipeViews}{" "}
+        </div>
       </div>
     </div>
   );
