f7fb357452fc7cfc3477467a8a2438eb0b4cdf62
Naufal Ibrahim <naufal.ti@outlook.com>
2022-06-29 09:13:32 +0700

    Edit when not logged it's "please login"

--- __tests__/profile/profileInfoCard.test.js
+++ __tests__/profile/profileInfoCard.test.js
@@ -1,41 +1,55 @@
 import { ProfileInfo } from "../../components/ProfilePage/ProfileInfo";
-import { screen,render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+describe("ProfileCard", () => {
+  const data = {
+    response: {
+      fullName: "Faris Adam",
+      totalRecipes: "2",
+      recipeLikes: "0",
+      followers: "2",
+    },
+  };
+  const session = {
+    user: {
+      id: 9,
+    },
+  };
 
-describe ("ProfileCard", () => {
-    const data = {
-        response :{
-            fullName: "Faris Adam",
-            totalRecipes:"2",
-            recipeLikes:"0",
-            followers:"2"
-        }
-    }
-    const session ={
-        user:{
-            id:9
-        }
-    }
+  it("should render the profile info card with data from props", () => {
+    render(<ProfileInfo userData={data} />);
+    expect(screen.getByText("Your Profile")).toBeInTheDocument();
+    expect(screen.getByText(data.response.fullName)).toBeInTheDocument();
+    expect(
+      screen.getByText(data.response.totalRecipes + " recipes")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(data.response.recipeLikes + " likes")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(data.response.followers + " followers")
+    ).toBeInTheDocument();
+  });
 
-    it ("should render the profile info card with data from props", () => {
-        render(<ProfileInfo userData={data} />);
-        expect(screen.getByText('Your Profile')).toBeInTheDocument();
-        expect(screen.getByText(data.response.fullName)).toBeInTheDocument();
-        expect(screen.getByText(data.response.totalRecipes + " recipes")).toBeInTheDocument();
-        expect(screen.getByText(data.response.recipeLikes + " likes")).toBeInTheDocument();
-        expect(screen.getByText(data.response.followers + " followers")).toBeInTheDocument();
-    })
+  it("should render edit profile", () => {
+    render(<ProfileInfo userData={data} session={session} creatorId={6} />);
+    expect(screen.getByText("Edit Profile")).toBeInTheDocument();
+  });
 
-    it ("should render edit profile", () => {        
-        render(<ProfileInfo userData={data} session={session} creatorId={6}/>);
-        expect(screen.getByText("Edit Profile")).toBeInTheDocument();
-    })
+  it("should render follow/unfollow button", () => {
+    const dataQueryParam = "faristest2";
 
-    it ("should render follow/unfollow button", () => {
-        const dataQueryParam = "faristest2"
-        
-        render(<ProfileInfo userData={data} dataQueryParam={dataQueryParam} session={session} creatorId={6}/>);
-        expect(screen.getByText("Unfollow")).toBeInTheDocument();
-    })
-})
+    render(
+      <ProfileInfo
+        userData={data}
+        dataQueryParam={dataQueryParam}
+        session={session}
+        creatorId={6}
+      />
+    );
+    expect(
+      screen.getByText("Please Login To Follow This Creator")
+    ).toBeInTheDocument();
+  });
+});
