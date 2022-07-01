a26ccba06ae8df9f268dc63093fa979a8ce40089
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-01 10:22:19 +0700

    Initiate table for user and change sturcture

--- components/Admin/UserManagement.tsx
+++ components/Admin/UserManagement.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import DataTable from "react-data-table-component";
+
+interface User {
+  id: number;
+  username: string;
+  fullname: string;
+  roles: string[];
+}
 
 export default function UserManagement() {
+  const [users, setUsers] = useState();
+  const columns = [
+    {
+      name: "Username",
+      sortFunction: (a: any, b: any) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      },
+      selector: (row: { id: any; name: string; temp: string }) => (
+        <div>username</div>
+      ),
+    },
+    {
+      name: "Roles",
+      sortable: false,
+      maxWidth: "100px",
+      selector: (row: { id: any; name: string; temp: string }) => {
+        return <div>roles</div>;
+      },
+    },
+  ];
   return (
-    <div>UserManagement</div>
-  )
+    <DataTable
+      name="Users"
+      //@ts-ignore
+      columns={columns}
+      // data={"test"}
+      progressPending={false}
+      pagination
+      paginationPerPage={5}
+      paginationRowsPerPageOptions={[5, 10, 20]}
+    />
+  );
 }
