3483e06cf202087e5fe25dee0a6665d4be892164
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-06 09:34:03 +0700

    Clicking tagpill increase ttl view of the recipe

--- components/TagsPill.tsx
+++ components/TagsPill.tsx
@@ -1,6 +1,7 @@
+import axios from "axios";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {addTagsToQuery,sendQuery,} from "../redux/reducers/queryReducer";
+import { useDispatch } from "react-redux";
+import { addTagsToQuery, sendQuery } from "../redux/reducers/queryReducer";
 
 const TagsPill = ({ tag }: any) => {
   const dispatch = useDispatch();
@@ -11,6 +12,7 @@ const TagsPill = ({ tag }: any) => {
       className="rounded py-1 px-4 mx-1 my-1 border-solid border-2 border-black transition-all cursor-pointer hover:scale-110 hover:bg-orange-300 "
       onClick={(event) => {
         dispatch(addTagsToQuery(tag));
+        axios.put(`${process.env.API_URL}tag/addview?tagId=${tag.id}`);
         //@ts-ignore
         dispatch(sendQuery());
       }}
