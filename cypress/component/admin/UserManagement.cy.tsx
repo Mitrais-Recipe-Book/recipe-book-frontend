be6a8ed76b22cb5bbc0ba3b3080436d734eb527f
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-06 15:37:10 +0700

    Add test to try add role

--- cypress/component/admin/UserManagement.cy.tsx
+++ cypress/component/admin/UserManagement.cy.tsx
@@ -8,8 +8,18 @@ describe("UserManagement", () => {
     cy.mount(<UserManagement />);
     cy.contains("Username");
   });
-  it.only("load 10 row as default", () => {
+  it("load 10 row as default", () => {
     cy.mount(<UserManagement />);
     cy.get(".sc-cxabCf").should("contain.text", "10");
+  });
+  it.only("try to add role", () => {
+    cy.mount(<UserManagement />);
+    cy.get(
+      '#cell-5-2 > [data-tag="allowRowEvents"] > .flex > .bg-green-600'
+    ).click();
+    cy.contains("Add Role");
+    cy.get(".swal2-select").select("Admin");
+    cy.get(".swal2-confirm").click();
+    cy.get(".swal2-confirm").click();
   });
 });
