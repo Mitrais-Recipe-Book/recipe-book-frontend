681a604a0efb932c2476f7b011a0462c4af5e881
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-07 15:40:16 +0700

    Test for tag mng: edit value

---
+++ cypress/component/admin/TagsTable.cy.tsx
@@ -0,0 +1,60 @@
+import TagsTable from "@components/Admin/TagsTable";
+
+describe("TagsTable", () => {
+  it("mount without error", () => {
+    cy.mount(<TagsTable />);
+  });
+
+  it("render tags table", () => {
+    cy.mount(<TagsTable />);
+    cy.contains("Tags");
+  });
+
+  it("show total recipes", () => {
+    cy.mount(<TagsTable />);
+    cy.contains("Total Recipes");
+  });
+
+  it("show total clicks", () => {
+    cy.mount(<TagsTable />);
+    cy.contains("Total Clicks");
+  });
+
+  it("can edit tag name", () => {
+    cy.mount(<TagsTable />);
+    cy.get(
+      '#cell-4-1 > [data-tag="allowRowEvents"] > div > .bg-blue-500'
+    ).click();
+    cy.get("#cell-1-1 > div > .w-full").should("not.be.disabled");
+  });
+
+  it("cannot change tag with same name", () => {
+    cy.mount(<TagsTable />);
+    cy.get(
+      '#cell-4-1 > [data-tag="allowRowEvents"] > div > .bg-blue-500'
+    ).click();
+    cy.get(
+      '#cell-4-1 > [data-tag="allowRowEvents"] > div > .bg-blue-500'
+    ).click();
+    cy.get(".block").should("contain.text", "Failed to edit tag");
+  });
+
+  it.only("can successfully change tag to other name", () => {
+    cy.mount(<TagsTable />);
+    cy.get(
+      '#cell-4-1 > [data-tag="allowRowEvents"] > div > .bg-blue-500'
+    ).click();
+    cy.get("#cell-1-1 > div > .w-full").clear().type("New Tag");
+    cy.get(
+      '#cell-4-1 > [data-tag="allowRowEvents"] > div > .bg-blue-500'
+    ).click();
+
+    cy.get(
+      '#cell-4-1 > [data-tag="allowRowEvents"] > div > .bg-blue-500'
+    ).click();
+    cy.get("#cell-1-1 > div > .w-full").clear().type("breakfast");
+    cy.get(
+      '#cell-4-1 > [data-tag="allowRowEvents"] > div > .bg-blue-500'
+    ).click();
+  });
+});
