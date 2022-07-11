676829ff0c5d915353274ad3b416bc6996c69c1d
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-11 10:54:07 +0700

    Add formik to change name and email

--- pages/edit-profile.tsx
+++ pages/edit-profile.tsx
@@ -1,5 +1,7 @@
 import Footer from "@components/Footer";
 import Navbar from "@components/Navbar";
+import { Form, Formik, Field } from "formik";
+import * as Yup from "yup";
 import React from "react";
 
 export default function editprofile() {
@@ -12,7 +14,50 @@ export default function editprofile() {
             <h1 className="text-4xl text-center mb-3 font-bold">
               Edit Profile
             </h1>
-            <div></div>
+            <Formik
+              initialValues={{
+                email: "",
+                fullName: "",
+              }}
+              onSubmit={(values) => {
+                console.log("first");
+                console.log(values);
+              }}
+              validationSchema={Yup.object({
+                email: Yup.string().email("Please fill a valid email"),
+                fullName: Yup.string().required("Full name is required"),
+              })}
+            >
+              {(props) => (
+                <div className="py-4 px-10">
+                  <Form>
+                    <div className="grid grid-cols-4 gap-2">
+                      <label className="grid-col-1">Email</label>
+                      <Field
+                        name="email"
+                        as="input"
+                        className=" bg-white text-gray-700 border border-gray-400 p-2 rounded grow col-span-3"
+                      />
+                      <label className="grid-col-1">Fullname</label>
+                      <Field
+                        name="fullName"
+                        as="input"
+                        className=" bg-white text-gray-700 border border-gray-400 p-2 rounded grow col-span-3"
+                      />
+                    </div>
+                    <button
+                      className="bg-red-500 hover:bg-red-700 disabled:bg-gray-500 text-white font-bold py-1 px-8 rounded cursor-pointer disabled:cursor-default"
+                      type="submit"
+                      disabled={
+                        !props.isValid || props.isSubmitting || !props.dirty
+                      }
+                    >
+                      SAVE
+                    </button>
+                  </Form>
+                </div>
+              )}
+            </Formik>
           </section>
         </div>
       </main>
