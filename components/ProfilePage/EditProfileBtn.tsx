d7d27c57878dbb2e5ba936d405ad9d07efd12795
Faris Adam <farisadam97@gmail.com>
2022-07-12 09:58:24 +0700

    edit btn profile info redirect to edit profile page

--- components/ProfilePage/EditProfileBtn.tsx
+++ components/ProfilePage/EditProfileBtn.tsx
@@ -1,5 +1,8 @@
+import { useRouter } from "next/router";
+
 export default function EditProfileBtn(props: any) {
+    const router = useRouter()
     return (
-        <button className="uppercase transition  bg-gray-800 text-sm text-white hover:bg-gray-600 px-4 py-2 rounded-md ">Edit Profile </button>
+        <button className="uppercase transition  bg-gray-800 text-sm text-white hover:bg-gray-600 px-4 py-2 rounded-md " onClick={()=> {router.push("/edit-profile")}}>Edit Profile </button>
     );
 }
