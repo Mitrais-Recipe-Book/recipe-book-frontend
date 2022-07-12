1a90f0143a57858108ff93784f6496f0242da396
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-12 07:49:23 +0700

    Consume API to get init value and edit value

--- pages/edit-profile.tsx
+++ pages/edit-profile.tsx
@@ -2,13 +2,33 @@ import Footer from "@components/Footer";
 import Navbar from "@components/Navbar";
 import { Form, Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChangePP from "@components/EditProfile/ChangePP";
 import { useSession } from "next-auth/react";
 import ChangePassword from "@components/EditProfile/ChangePassword";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+interface UserInfo {
+  id: number;
+  email: string;
+  username: string;
+  fullName: string;
+  roles: string[];
+}
 
 export default function editprofile() {
   const { data: session }: any = useSession();
+  const [userInfo, setUserInfo] = useState<UserInfo>();
+  useEffect(() => {
+    if (session) {
+      axios
+        .get(`${process.env.API_URL}user/${session?.user.username}/profile`)
+        .then((res) => {
+          setUserInfo(res.data.payload);
+        });
+    }
+  }, [session]);
   return (
     <>
       <Navbar />
@@ -18,64 +38,78 @@ export default function editprofile() {
             <h1 className="text-4xl text-center mb-3 font-bold">
               Edit Profile
             </h1>
-            <Formik
-              initialValues={{
-                email: "",
-                fullName: "",
-              }}
-              onSubmit={(values) => {
-                console.log("first");
-                console.log(values);
-              }}
-              validationSchema={Yup.object({
-                email: Yup.string().email("Please fill a valid email"),
-                fullName: Yup.string().required("Full name is required"),
-              })}
-            >
-              {(props) => (
-                <div className="py-4 px-10">
-                  <ChangePP username={session?.user.username} />
-                  <Form>
-                    <div className="grid grid-cols-4 gap-2 my-4">
-                      <label className="grid-col-1">Email</label>
-                      <Field
-                        name="email"
-                        as="input"
-                        className=" bg-white text-gray-700 border border-gray-400 p-2 rounded grow col-span-3"
-                      />
-                      <ErrorMessage
-                        name="email"
-                        component="div"
-                        className="col-span-4 text-red-500"
-                      />
+            {userInfo ? (
+              <Formik
+                initialValues={{
+                  email: userInfo?.email,
+                  fullName: userInfo?.fullName,
+                }}
+                onSubmit={(values) => {
+                  axios
+                    .put(
+                      `${process.env.API_URL}user/${session?.user.username}/profile`,
+                      values
+                    )
+                    .then((res) => {
+                      Swal.fire({
+                        title: "Success",
+                        text: "Profile updated successfully",
+                        icon: "success",
+                      });
+                    });
+                }}
+                validationSchema={Yup.object({
+                  email: Yup.string().email("Please fill a valid email"),
+                  fullName: Yup.string().required("Full name is required"),
+                })}
+              >
+                {(props) => (
+                  <div className="py-4 px-10">
+                    <ChangePP username={session?.user.username} />
+                    <Form>
+                      <div className="grid grid-cols-4 gap-2 my-4">
+                        <label className="grid-col-1">Email</label>
+                        <Field
+                          name="email"
+                          as="input"
+                          className=" bg-white text-gray-700 border border-gray-400 p-2 rounded grow col-span-3"
+                        />
+                        <ErrorMessage
+                          name="email"
+                          component="div"
+                          className="col-span-4 text-red-500"
+                        />
 
-                      <label className="grid-col-1">Fullname</label>
-                      <Field
-                        name="fullName"
-                        as="input"
-                        className=" bg-white text-gray-700 border border-gray-400 p-2 rounded grow col-span-3"
-                      />
-                      <ErrorMessage
-                        name="fullName"
-                        component="div"
-                        className="col-span-4 text-red-500"
-                      />
-                      <label className="grid-scol-1">Change Password</label>
-                      <ChangePassword />
-                    </div>
-                    <button
-                      className="bg-red-500 hover:bg-red-700 disabled:bg-gray-500 text-white font-bold py-1 px-8 rounded cursor-pointer disabled:cursor-default"
-                      type="submit"
-                      disabled={
-                        !props.isValid || props.isSubmitting || !props.dirty
-                      }
-                    >
-                      SAVE
-                    </button>
-                  </Form>
-                </div>
-              )}
-            </Formik>
+                        <label className="grid-col-1">Fullname</label>
+                        <Field
+                          name="fullName"
+                          as="input"
+                          className=" bg-white text-gray-700 border border-gray-400 p-2 rounded grow col-span-3"
+                        />
+                        <ErrorMessage
+                          name="fullName"
+                          component="div"
+                          className="col-span-4 text-red-500"
+                        />
+                        <label className="grid-scol-1">Change Password</label>
+                        <ChangePassword />
+                      </div>
+                      <button
+                        className="bg-red-500 hover:bg-red-700 disabled:bg-gray-500 text-white font-bold py-1 px-8 rounded cursor-pointer disabled:cursor-default"
+                        type="submit"
+                        disabled={
+                          !props.isValid || props.isSubmitting || !props.dirty
+                        }
+                      >
+                        SAVE
+                      </button>
+                    </Form>
+                  </div>
+                )}
+              </Formik>
+            ) : (
+              <div className="py-4 px-10">Loading...</div>
+            )}
           </section>
         </div>
       </main>
