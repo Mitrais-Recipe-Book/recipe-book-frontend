1322f639966c910f4eacab96f1b98fd5e33c3537
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-19 10:31:00 +0700

    Template for request page

---
+++ components/Admin/RequestTable.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import DataTable from "react-data-table-component";
+
+interface User {
+  id: number;
+  email: string;
+  username: string;
+  fullName: string;
+  roles: string[];
+}
+interface Props {
+  data: User[];
+  loading: boolean;
+  totalRows: number;
+  fetchUser: (page: number) => void;
+}
+
+export default function RequestTable(props: Props) {
+  const columns = [
+    {
+      name: "Username",
+      sortable: true,
+      style: { marginLeft: "20%" },
+      selector: (row: { username: string }) => <div>{row.username}</div>,
+    },
+    {
+      name: "Request",
+      sortable: false,
+      style: { marginLeft: "10%" },
+      selector: (row: { username: string }) => {
+        return (
+          <div className="flex gap-4">
+            <button className="bg-green-600 text-white px-2 py-1 rounded">
+              Accept
+            </button>
+            <button className="bg-red-600 text-white px-2 py-1 rounded">
+              Reject
+            </button>
+          </div>
+        );
+      },
+    },
+  ];
+  const customStyles = {
+    headCells: {
+      style: {
+        marginLeft: "20%",
+      },
+    },
+  };
+  return (
+    <DataTable
+      name="Request"
+      //@ts-ignore
+      columns={columns}
+      data={props.data}
+      progressPending={props.loading}
+      noRowsPerPage={true}
+      customStyles={customStyles}
+      pagination
+      paginationServer
+      paginationTotalRows={props.totalRows}
+      paginationPerPage={10}
+      onChangePage={props.fetchUser}
+    />
+  );
+}
--- pages/admin/request.tsx
+++ pages/admin/request.tsx
@@ -1,49 +1,42 @@
-import React from "react";
-import DataTable from "react-data-table-component";
+import RequestTable from "@components/Admin/RequestTable";
+import Footer from "@components/Footer";
+import Navbar from "@components/Navbar";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function request() {
-  const dummyData = [
-    {
-      username: "John",
-    },
-  ];
-  const columns = [
-    {
-      name: "Username",
-      sortFunction: (a: any, b: any) => {
-        if (a.name < b.name) {
-          return -1;
-        }
-        if (a.name > b.name) {
-          return 1;
-        }
-        return 0;
-      },
-      selector: (row: { username: string }) => <div>{row.username}</div>,
-    },
-    {
-      name: "Request",
-      sortable: false,
-      selector: (row: { username: string }) => {
-        return (
-          <div>
-            <button className="px-5">Accept</button>
-            <button className="px-5">Reject</button>
-          </div>
-        );
-      },
-    },
-  ];
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [totalRows, setTotalRows] = useState(0);
+
+  async function fetchUser(page: number) {
+    setLoading(true);
+    const response = await axios.get(
+      `${process.env.API_URL}user/role-request/{page}?page=0`
+    );
+    setData(response.data.payload.data);
+    setTotalRows(response.data.payload.totalItem - 1);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchUser(0);
+  }, []);
   return (
-    <DataTable
-      name="Request"
-      //@ts-ignore
-      columns={columns}
-      data={dummyData}
-      progressPending={false}
-      pagination
-      paginationPerPage={5}
-      paginationRowsPerPageOptions={[5, 10, 20]}
-    />
+    <>
+      <Navbar />
+      <div className="container mx-auto mt-10 w-3/4">
+        <h1 className="text-center text-2xl mb-4">Reuqest Table</h1>
+        <div className="flex flex-col justify-items-center ">
+          <RequestTable
+            data={data}
+            loading={loading}
+            totalRows={totalRows}
+            fetchUser={fetchUser}
+          />
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
