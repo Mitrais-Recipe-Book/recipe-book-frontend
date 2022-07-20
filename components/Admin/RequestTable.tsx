d2a3ddf82535c10b38b607a243383795a75dfb39
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-20 09:17:01 +0700

    Add function to approve user request

--- components/Admin/RequestTable.tsx
+++ components/Admin/RequestTable.tsx
@@ -1,5 +1,8 @@
+import { removeUser } from "@redux/reducers/userReducer";
+import axios from "axios";
 import React from "react";
 import DataTable from "react-data-table-component";
+import Swal from "sweetalert2";
 
 interface User {
   id: number;
@@ -11,9 +14,27 @@ interface User {
 interface Props {
   data: User[];
   loading: boolean;
+  removeUser(username: string): void;
 }
 
 export default function RequestTable(props: Props) {
+  function acceptRequest(username: string): void {
+    axios
+      .put(`${process.env.API_URL}user/${username}/approve-creator`)
+      .then(() => {
+        props.removeUser(username);
+        Swal.fire({
+          title: "Success",
+          text: "Request has been approved",
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+      });
+  }
+
+  function rejectRequest(username: string): void {
+    console.log("Reject ", username);
+  }
   const columns = [
     {
       name: "Username",
@@ -28,10 +49,16 @@ export default function RequestTable(props: Props) {
       selector: (row: { username: string }) => {
         return (
           <div className="flex gap-4">
-            <button className="bg-green-600 text-white px-2 py-1 rounded">
+            <button
+              className="bg-green-600 text-white px-2 py-1 rounded"
+              onClick={() => acceptRequest(row.username)}
+            >
               Accept
             </button>
-            <button className="bg-red-600 text-white px-2 py-1 rounded">
+            <button
+              className="bg-red-600 text-white px-2 py-1 rounded"
+              onClick={() => rejectRequest(row.username)}
+            >
               Reject
             </button>
           </div>
