43507527e58602c12ff0895ff0362a21ce6bf862
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-21 09:06:31 +0700

    Consume last 10 view recipes

--- pages/recent-view.tsx
+++ pages/recent-view.tsx
@@ -1,7 +1,9 @@
 import Footer from "@components/Footer";
 import Navbar from "@components/Navbar";
 import RecipeCard from "@components/RecipeCard";
-import React from "react";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
 
 interface Recipe {
   id: number;
@@ -15,25 +17,54 @@ interface Recipe {
   };
 }
 
-export default function recentview() {
-  const recipeDummy = {
-    id: 1,
-    recipeName: "Es Teh Anget (edit)",
-    description: "Enak diminum untuk buka puasa",
-    recipeViews: 108,
-    author: {
-      username: "user1",
-      fullName: "rick",
-      authorFollowers: 10,
-    },
+interface Session {
+  expires: string;
+  user: {
+    id: number;
+    username: string;
+    fullName: string;
+    email: string;
+    roles: string[];
   };
+}
+
+export default function recentview() {
+  const { data: session }: any = useSession();
+  const [recipes, setRecipes] = useState<Recipe[]>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (session) {
+      fetchData();
+    }
+  }, [session]);
+
+  function fetchData() {
+    axios
+      .get(
+        `${process.env.API_URL}recipe/viewed?username=${session?.user.username}&isPaginated=false&page=0&size=10`
+      )
+      .then((res) => {
+        setRecipes(res.data.payload.data);
+        setLoading(false);
+      });
+  }
+
+  function mapData(recipes: Recipe[] | undefined): JSX.Element[] | JSX.Element {
+    if (recipes) {
+      return recipes!.map((recipe: Recipe) => {
+        return <RecipeCard recipe={recipe} />;
+      });
+    } else {
+      return <div>No Recipes</div>;
+    }
+  }
+
   return (
     <>
       <Navbar />
       <main className="container mx-auto pt-1">
-        <div>
-          <RecipeCard recipe={recipeDummy} />
-        </div>
+        <div>{loading ? <div>Loading...</div> : mapData(recipes)}</div>
       </main>
       <Footer />
     </>
