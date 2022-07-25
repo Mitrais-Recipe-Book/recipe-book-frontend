a38d8c38e2fa140efe2af11565eb2b961f140887
Naufal Ibrahim <naufal.ti@outlook.com>
2022-07-25 08:41:51 +0700

    Add remove role testing

--- cypress/component/admin/UserManagement.cy.tsx
+++ cypress/component/admin/UserManagement.cy.tsx
@@ -20,6 +20,24 @@ describe("UserManagement", () => {
     cy.contains("Add Role");
     cy.get(".swal2-select").select("Admin");
     cy.get(".swal2-confirm").click();
+    cy.contains("Role added successfully");
+    cy.get(".swal2-confirm").click();
+  });
+  it("cannot add the same role", () => {
+    cy.mount(<UserManagement />);
+    cy.get(
+      '#cell-5-2 > [data-tag="allowRowEvents"] > .flex > .bg-green-600'
+    ).click();
+    cy.contains("Add Role");
+    cy.get(".swal2-select").select("Admin");
+    cy.get(".swal2-confirm").click();
+    cy.contains("User already has this role");
+    cy.get(".swal2-confirm").click();
+  });
+  it("remove role", () => {
+    cy.mount(<UserManagement />);
+    cy.get('#cell-4-2 > [data-tag="allowRowEvents"] > .cursor-pointer').click();
+    cy.contains("remove Admin role");
     cy.get(".swal2-confirm").click();
   });
 });
